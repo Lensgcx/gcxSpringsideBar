671530841a07bba8b74f589ba32350438b2ee302
BroccoliSpring <Lensgcx@163.com>
2017-09-04 19:40:22 +0800

    skin - initskin

--- src/js/skin/initskin.js
+++ src/js/skin/initskin.js
@@ -5,7 +5,6 @@
  * Created by Administrator on 2017/3/14.
  */
 /* ===========================================================
- * ru.js
  * Russion translation for Trumbowyg
  * http://alex-d.github.com/Trumbowyg
  * ===========================================================
